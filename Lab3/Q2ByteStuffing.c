2724bff8bf54e369d45215cad148668dc6a75072
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-08-17 09:11:58 +0530

    Added the destuff part

--- Lab3/Q2ByteStuffing.c
+++ Lab3/Q2ByteStuffing.c
@@ -1,85 +1,85 @@
 // Wap to demonstrate byte stuffing and destuffing for a given frame(string) input by the user.
+
 #include <stdio.h>
 #include <string.h>
-
-int main()
+void main()
 {
-    char frame[50][50], str[50][50];
+    char frame[50][50], str[50][50], destuffed[50][50];
     char flag[10];
     strcpy(flag, "flag");
     char esc[10];
     strcpy(esc, "esc");
     int i, j, k = 0, n;
-
-    printf("Enter the number of strings: ");
+    strcpy(frame[k++], "flag");
+    printf("Enter length of String : \n");
     scanf("%d", &n);
-    getchar();
-
-    printf("Enter the strings:\n");
-    for (i = 0; i < n; i++)
+    printf("Enter the String:\n ");
+    for (i = 0; i <= n; i++)
     {
-        fgets(str[i], sizeof(str[i]), stdin);
-        str[i][strcspn(str[i], "\n")] = '\0';
+        gets(str[i]);
     }
-
-    printf("You Entered:\n");
-    for (i = 0; i < n; i++)
+    printf("\nYou entered :\n");
+    for (i = 0; i <= n; i++)
     {
         puts(str[i]);
     }
-
-    for (i = 0; i < n; i++)
-    {
-        strcpy(frame[k++], esc);
-        for (j = 0; j < strlen(str[i]); j++)
-        {
-            if (str[i][j] == esc[0] || str[i][j] == flag[0])
-            {
-                strcpy(frame[k++], esc);
-            }
-            char temp[2] = {str[i][j], '\0'};
-            strcpy(frame[k++], temp);
-        }
-        strcpy(frame[k++], esc);
-    }
-
-    printf("\n-------------------------------\n");
-    printf("Byte stuffing at sender side:\n\n");
-    printf("--------------------------------\n");
-    for (i = 0; i < k; i++)
-    {
-        printf("%s ", frame[i]);
-    }
     printf("\n");
-
-    printf("-------------------------------\n");
-    printf("Byte destuffing at receiver side:\n\n");
-    printf("--------------------------------\n");
-
-    i = 0;
-    while (i < k)
+    for (i = 1; i <= n; i++)
     {
-        if (strcmp(frame[i], esc) == 0)
+        if (strcmp(str[i], flag) != 0 && strcmp(str[i], esc) != 0)
         {
-            i++;
-            if (strcmp(frame[i], esc) == 0)
-            {
-                printf("%s ", esc);
-                i++;
-            }
-            else
-            {
-                printf("%s", frame[i]);
-                i++;
-            }
+            strcpy(frame[k++], str[i]);
         }
         else
         {
-            printf("%s ", frame[i]);
-            i++;
+            strcpy(frame[k++], "esc");
+            strcpy(frame[k++], str[i]);
         }
     }
-
+    strcpy(frame[k++], "flag");
+    printf("------------------------------\n\n");
+    printf("Byte stuffing at sender side:\n\n");
+    printf("------------------------------\n\n");
+    for (i = 0; i < k; i++)
+    {
+        printf("%s\t", frame[i]);
+    }
     printf("\n");
-    return 0;
+
+    // Destuffing
+    k = 0;
+
+    while (strcmp(frame[k], "flag") != 0)
+    {
+        k++;
+    }
+    k++;
+
+    i = 0; // Counter for destuffed frame
+    while (k < n)
+    {
+        if (strcmp(frame[k], "esc") == 0)
+        {
+            k++;
+            strcpy(destuffed[i++], frame[k]);
+        }
+        else if (strcmp(frame[k], "flag") == 0)
+        {
+            break; // End of frame
+        }
+        else
+        {
+            strcpy(destuffed[i++], frame[k]);
+        }
+        k++;
+    }
+
+    // Display Destuffed Frame
+    printf("------------------------------\n");
+    printf("Byte destuffing at receiver side:\n");
+    printf("------------------------------\n");
+    for (j = 0; j < i; j++)
+    {
+        printf("%s\t", destuffed[j]);
+    }
 }
